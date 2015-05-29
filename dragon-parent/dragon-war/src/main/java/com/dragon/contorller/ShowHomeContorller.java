715af8c31eabf5c92093deda3fc33df97dba707b
sunshinetsl <sunshinetsl@163.com>
2015-05-29 14:20:45 +0800

    Signed-off-by: sunshinetsl <sunshinetsl@163.com>

--- dragon-parent/dragon-war/src/main/java/com/dragon/contorller/ShowHomeContorller.java
+++ dragon-parent/dragon-war/src/main/java/com/dragon/contorller/ShowHomeContorller.java
@@ -1,7 +1,6 @@
 package com.dragon.contorller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
