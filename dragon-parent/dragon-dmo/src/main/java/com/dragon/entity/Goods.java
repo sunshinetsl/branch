37c715fcacc25d2ae77dff30b5f27f26ae62d2d5
sunshinetsl <sunshinetsl@163.com>
2015-05-29 15:25:03 +0800

    Signed-off-by: sunshinetsl <sunshinetsl@163.com>

--- dragon-parent/dragon-dmo/src/main/java/com/dragon/entity/Goods.java
+++ dragon-parent/dragon-dmo/src/main/java/com/dragon/entity/Goods.java
@@ -12,6 +12,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 
+import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.GenericGenerator;
 
 
