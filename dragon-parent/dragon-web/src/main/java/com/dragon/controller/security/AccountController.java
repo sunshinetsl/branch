3476ba540501a42df820a8b0759f3f73fb813ab0
sunshinetsl <sunshinetsl@163.com>
2015-06-05 17:37:17 +0800

    添加用户管理

--- dragon-parent/dragon-web/src/main/java/com/dragon/controller/security/AccountController.java
+++ dragon-parent/dragon-web/src/main/java/com/dragon/controller/security/AccountController.java
@@ -1,5 +1,7 @@
 package com.dragon.controller.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,12 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class AccountController {
 
+	private Logger logger = LoggerFactory.getLogger(AccountController.class);
 	/**
 	 * 注册页面
 	 * @return
 	 */
 	@RequestMapping("regPage")
 	public String regPage(){
+		if(logger.isDebugEnabled()){
+			logger.debug("regPage -->start");
+		}
 		return null;
 	}
 }
